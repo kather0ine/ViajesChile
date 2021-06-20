5665467ac182fd425b3c38c0cb90b3ad3ec613a6
kath <holakather0ine@gmail.com>
2021-06-20 12:59:15 -0400

    Actualizacion de js y html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,13 @@
-alert("vamo q c puede");
+$(function(){
+//popover
 
-$(document).ready(function(){
-    //$('[data-toggle="tooltip"]').tooltip();  
-
-$("a").on('click', function(){
-    if (this.hash !== "") {
-        var hash= this.hash;
-
-        $('html,body').animate({
-            scrollTop: $(hash).offset().top}, 900, function() 
-            {
-            window.location.hash = hash;
-
-        });
-    }
+    $('[data-toggle="popover"]').popover()
 });
-})
 
+$("#destacados").click(function(){
+    $(this).css({
+        "color": "yellow",
+    });
+  
+  });
+  
