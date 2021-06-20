d6c0f12e13b759d4d5a97e4f4aa91480541c3177
kath <holakather0ine@gmail.com>
2021-06-20 13:16:39 -0400

    Actualizacion de JS :')

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,19 @@
 $(function(){
+    
 //popover
 
     $('[data-toggle="popover"]').popover()
 });
 
+//Evento CSS
 $("#destacados").click(function(){
     $(this).css({
         "color": "yellow",
     });
   
   });
-  
+
+//Selector de clase
+$(".card-img-top, .boton").click(function () {
+    $(".card-body").toggle("slow")
+  })
